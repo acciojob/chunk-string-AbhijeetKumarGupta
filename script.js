a99cf1b1f045da13d3c377e2642d96f221e61700
Abhijeet Kumar Gupta <84043354+AbhijeetKumarGupta@users.noreply.github.com>
2026-08-15 23:09:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
   // your code here
+  const noOfChunks = Math.ceil(str.length/size);
+  const chunks = new Array(noOfChunks);
+
+  for(
+    let chunkNo = 0, start = 0; 
+    chunkNo < noOfChunks; 
+    chunkNo++, start += size
+  ){
+    chunks[chunkNo] = str.substring(start, start+size);
+  }
+
+  return chunks;
 }
 
 // Do not change the code below
